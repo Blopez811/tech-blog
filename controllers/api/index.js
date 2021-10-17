d86d26fe6db434858a3beecc24ee30d7236a0134
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-17 12:57:59 -0500

    added username get one and post routes. Added th login route as well

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,6 +6,6 @@ const commentRoutes = require('./comment-routes');
 // /api/what you write below
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes)
+router.use('/comments', commentRoutes);
 
 module.exports = router;
