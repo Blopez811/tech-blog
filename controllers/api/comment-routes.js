8fd1cc9d34bd2277ea270ff1dbbdda0eb8e4123c
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-17 12:32:38 -0500

    added a get all route for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'user_id', 'post_id']
+    })
+        .then(dbCommentData => {
+            res.json(dbCommentData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+
+});
+
+module.exports = router;
