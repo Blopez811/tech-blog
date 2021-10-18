f1b0d458cfbb8e041bfa9cab9de1898c4bea2548
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 11:01:27 -0500

    user's previous posts now display on their dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -50,7 +50,41 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/dashboard', withAuth, (req,res) => {
-    res.render('dashboard')
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'body',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
