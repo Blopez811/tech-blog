28097e8701a6ca5e6ba80a83b041e8abe7fa89e6
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-17 20:01:07 -0500

    login page now renders and makes a post request and logs user in, at which point they are redirected to the dashboard page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,6 +8,14 @@ router.get('/', (req, res) => {
 
 router.get('/signup', (req, res) => {
     res.render('signup')
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+router.get('/dashboard', (req,res) => {
+    res.json('Here is the glorious dashboard page!')
 })
 
 module.exports = router;
