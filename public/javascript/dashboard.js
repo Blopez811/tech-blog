4deccef930030b818189cc010dea17a8167a44e2
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 10:41:16 -0500

    save changes button on modal now posts new user's post to the database and also to the homepage as well

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -2,7 +2,7 @@ const newPostBtn = document.querySelector('#newPostBtn');
 const myModal = document.querySelector('#myModal');
 const xModalBtn = document.querySelector('#xModalBtn');
 const closeBtn = document.querySelector('#closeBtn');
-
+const saveBtn = document.querySelector('#saveBtn');
 function newPostHandler() {
     console.log('create post fired!');
     myModal.style.display = "contents";
@@ -10,8 +10,34 @@ function newPostHandler() {
 
 function exitModal() {
     myModal.style.display = "none";
+};
+
+async function saveBtnHandler() {
+    event.preventDefault();
+  
+    const title = document.querySelector('#titleInput').value;
+    const body = document.querySelector('#bodyInput').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+      console.log('successful post route');
+    } else {
+      alert(response.statusText);
+    }
 }
 
 newPostBtn.addEventListener('click', newPostHandler);
 xModalBtn.addEventListener('click', exitModal);
-closeBtn.addEventListener('click', exitModal)
+closeBtn.addEventListener('click', exitModal);
+saveBtn.addEventListener('click', saveBtnHandler);
