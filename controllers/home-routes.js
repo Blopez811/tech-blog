9db97c6e18c3b619adf46e88405e3c7aa35d782a
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 00:06:58 -0500

    login/logout buttons work correctly. Blog posts titles now display on the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,45 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth')
 
 router.get('/', (req, res) => {
-    res.render('homepage')
-});
+    console.log(req.session);
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        console.log(dbPostData[0]);
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 router.get('/signup', (req, res) => {
     res.render('signup')
