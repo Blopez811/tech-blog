c6c4f0adfeb15345f604691d6b0b4b260fcb1926
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 10:25:58 -0500

    close button now also exits out of the modal

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,6 +1,8 @@
 const newPostBtn = document.querySelector('#newPostBtn');
 const myModal = document.querySelector('#myModal');
 const xModalBtn = document.querySelector('#xModalBtn');
+const closeBtn = document.querySelector('#closeBtn');
+
 function newPostHandler() {
     console.log('create post fired!');
     myModal.style.display = "contents";
@@ -11,4 +13,5 @@ function exitModal() {
 }
 
 newPostBtn.addEventListener('click', newPostHandler);
-xModalBtn.addEventListener('click', exitModal)
+xModalBtn.addEventListener('click', exitModal);
+closeBtn.addEventListener('click', exitModal)
