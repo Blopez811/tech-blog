5a1517552b05859b88060bc98c22caa4dddf31f3
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 10:06:11 -0500

    create new post button now pops up appropriate modal that takes in the title and body input from the user

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,7 +1,8 @@
 const newPostBtn = document.querySelector('#newPostBtn');
-
+const myModal = document.querySelector('#myModal');
 function newPostHandler() {
-    console.log('create post fired!')
+    console.log('create post fired!');
+    myModal.style.display = "contents";
 }
 
 newPostBtn.addEventListener('click', newPostHandler);
