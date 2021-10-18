ebefaedb58b9b89581335a330ce273f572508c1f
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-18 10:21:53 -0500

    X button in modal now closes out the modal

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -1,8 +1,14 @@
 const newPostBtn = document.querySelector('#newPostBtn');
 const myModal = document.querySelector('#myModal');
+const xModalBtn = document.querySelector('#xModalBtn');
 function newPostHandler() {
     console.log('create post fired!');
     myModal.style.display = "contents";
+};
+
+function exitModal() {
+    myModal.style.display = "none";
 }
 
 newPostBtn.addEventListener('click', newPostHandler);
+xModalBtn.addEventListener('click', exitModal)
