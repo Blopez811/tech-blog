be28912ff7c0512794400579a0c54459bc81a87d
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-17 19:41:33 -0500

    signup page is now working and posting new accounts to the database

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,8 +1,32 @@
 const signupBtn = document.querySelector('#signupBtn');
 
-function signupHandler() {
-    console.log('singupHandler function fired!')
-}
+async function signupHandler() {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        console.log(response);
+
+        //check the response status
+        if(response.ok) {
+            console.log('succes');
+            alert('Succesful account creation! You are ready to login at the login page!')
+        } else {
+            alert(respsonse.statusText);
+        }
+    }
+};
+
 
 
 signupBtn.addEventListener('click', signupHandler )
