50f8d9d79152b5a2edc032a6d867cd7c71cba053
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-24 16:17:31 -0500

    logoutTime function now fires at start of every page if logged in

---
+++ public/javascript/setTime.js
@@ -0,0 +1,8 @@
+function logoutTime() {
+     // Set a time frame countdown using SetTimeOut()
+     //Set event listener to watch for entire window document for a click or action, which will reset timer if occurs
+     // When time runs down we fire logout route to end session.
+    console.log('logoutTimer from setTime.js fired!');
+}
+
+logoutTime();
--- utils/helpers.js
+++ utils/helpers.js
@@ -4,11 +4,4 @@ module.exports = {
             date
         ).getFullYear()}`;
     },
-
-    logoutTimer: () => {
-        // Set a time frame countdown using SetTimeOut()
-        //Set event listener to watch for entire window document for a click or action, which will reset timer if occurs
-        // When time runs down we fire logout route to end session.
-        console.log("logoutTimer fired!")
-    }
 }
