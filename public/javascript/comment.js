e5e122530fbdc4806886f55b1f69efbe37a084f3
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-24 10:12:50 -0500

    comment post route for single post page works now

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,8 +1,31 @@
-const addCommentInput = document.querySelector('#addCommentInput');
+const addCommentInputEl = document.querySelector('#addCommentInput');
 const addCommentBtn = document.querySelector('#addCommentBtn');
+let post_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+];
+async function addCommentHandler() {
+    console.log('addCommentHandler fired!');
+    console.log(addCommentInputEl.value);
+    let comment_text = addCommentInputEl.value
+    event.preventDefault();
+    if(comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-function addCommentHandler() {
-    console.log('addCommentHandler fired!')
+        if(response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
 };
 
 addCommentBtn.addEventListener('click', addCommentHandler);
