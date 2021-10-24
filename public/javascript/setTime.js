59f74f2cc41f27cd4e7dda21a3b16d2d69c6b23d
Benicio Lopez <benicio.lopez@gmail.com>
2021-10-24 16:26:33 -0500

    setTimeout function now logs user out and redirects them to the login page.

--- public/javascript/setTime.js
+++ public/javascript/setTime.js
@@ -1,5 +1,22 @@
 function logoutTime() {
      // Set a time frame countdown using SetTimeOut()
+     setTimeout(function(){ 
+        async function logout () {
+            const response = await fetch('/api/users/logout', {
+                method: 'post',
+                headers: {'Content-Type': 'application/json'}
+            });
+        
+            if(response.ok) {
+                document.location.replace('/login');
+                alert("You were logged out due to inactivity, please log in again!")
+            } else {
+                alert(response.statusText);
+            }  
+        };
+        logout()
+        }, 3000);
+
      //Set event listener to watch for entire window document for a click or action, which will reset timer if occurs
      // When time runs down we fire logout route to end session.
     console.log('logoutTimer from setTime.js fired!');
